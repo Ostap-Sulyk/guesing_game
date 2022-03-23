1757422177cb73bbb66877499400a0a3a10a72ee
Ostap Sulyk <ostapsulyk@gmail.com>
2022-03-23 14:01:11 -0400

    added emoji

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
 use colored::*;
 use rand::Rng;
 use std::{cmp::Ordering, io};
-fn main() {
-    let secret_number = rand::thread_rng().gen_range(1, 101); // generating random number
 
+fn main() {
     println!("Guess the number!");
 
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
     loop {
-        println!("Secret number is {secret_number}");
+        // println!("Secret number is {secret_number}");
         // getting input
         let mut guess = String::new();
         println!("Please input your number");
-        io::stdin().read_line(&mut guess).expect("Cant read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Could not read the line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
+        let guess = match guess.trim().parse::<u32>() {
+            Ok(value) => value,
+            Err(_) => {
+                println!("{}", "You entered some gibberish 👾".red());
+                println!("{}", "Hint! Enter positive integer number 😉".yellow());
+                continue;
+            }
         };
 
-        println!("You guessed: {}", guess);
-
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("{}", "Too small".red()),
             Ordering::Greater => println!("{}", "Too big".red()),
+            Ordering::Less => println!("{}", "Too small".red()),
             Ordering::Equal => {
-                println!("{}", "You win!".green());
+                println!("{}", "You win 🙌 🏆".green());
                 break;
             }
         }
